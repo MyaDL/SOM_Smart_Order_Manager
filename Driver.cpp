c6eef84118a3c2c2d4a64a0ca326f64c8a74f592
Mya <mymy.underworld@gmail.com>
2023-04-10 15:59:25 +0200

    Classe Driver

--- Driver.cpp
+++ Driver.cpp
@@ -2,4 +2,58 @@
 // Created by Mya on 10/04/2023.
 //
 
+#include <list>
+#include <string>
+
 #include "Driver.h"
+
+
+//Constructeurs
+
+Driver::Driver(const std::string &lastname, const std::string &firstname, const std::string &address, int driverId,
+               const std::list<std::string> &driverTrip) : Person(lastname, firstname, address), driverId(driverId),
+                                                           driverTrip(driverTrip) {}
+
+Driver::Driver(const std::string &lastname, const std::string &firstname, const std::string &address) : Person(lastname,
+                                                                                                               firstname,
+                                                                                                               address) {}
+
+Driver::Driver(const std::string &lastname, const std::string &firstname, const std::string &address, int driverId)
+        : Person(lastname, firstname, address), driverId(driverId) {}
+
+
+//Destructeur
+
+Driver::~Driver() {
+
+}
+
+//Ajouter un trajet
+
+void Driver::addTrip(const std::string& Trip)
+{
+    Driver::driverTrip.push_back(Trip);
+}
+
+//Modifier un trajet
+
+void Driver::modifyTrip(const std::string &oldTrip, const std::string &newTrip)
+{
+    for(auto it = driverTrip.begin(); it != driverTrip.end(); ++it)
+    {
+        if(*it == oldTrip)
+        {
+            *it == newTrip;
+            return;
+        }
+    }
+}
+
+//Supprimer Trajet
+
+void Driver::deleteTrip(const std::string &Trip)
+{
+    driverTrip.remove(Trip);
+}
+
+
--- Driver.h
+++ Driver.h
@@ -11,7 +11,31 @@
 #include "Person.h"
 
 
-class Driver {
+class Driver : public Person {
+
+private:
+    int driverId;
+    std::list<std::string> driverTrip;
+
+public:
+
+    //Construteur
+
+    Driver(const std::string &lastname, const std::string &firstname, const std::string &address, int driverId,
+           const std::list<std::string> &driverTrip);
+
+    Driver(const std::string &lastname, const std::string &firstname, const std::string &address);
+
+    Driver(const std::string &lastname, const std::string &firstname, const std::string &address, int driverId);
+
+    //Destructeur
+
+    ~Driver() override;
+
+    void addTrip(const std::string& Trip);
+    void modifyTrip(const std::string& oldTrip, const std::string& newTrip);
+    void deleteTrip(const std::string& Trip);
+
 
 };
 
