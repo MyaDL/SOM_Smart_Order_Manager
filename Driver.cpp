19543c474470373c15beca1671ea877f8d508f01
Mya <mymy.underworld@gmail.com>
2023-04-10 15:38:57 +0200

    Classe Driver

---
+++ Driver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mya on 10/04/2023.
+//
+
+#include "Driver.h"
---
+++ Driver.h
@@ -0,0 +1,19 @@
+//
+// Created by Mya on 10/04/2023.
+//
+
+#ifndef SOM_SMART_ORDER_MANAGER_DRIVER_H
+#define SOM_SMART_ORDER_MANAGER_DRIVER_H
+
+#include <list>
+#include <string>
+
+#include "Person.h"
+
+
+class Driver {
+
+};
+
+
+#endif //SOM_SMART_ORDER_MANAGER_DRIVER_H
